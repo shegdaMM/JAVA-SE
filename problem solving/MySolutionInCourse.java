6c40ff1d3a99c7f63016dd5a9fc3c95089d4fec3
Shegda Mihail <shegdaMM@users.noreply.github.com>
2019-04-25 10:28:06 +0300

    Update MySolutionInCourse.java

--- problem solving/MySolutionInCourse.java
+++ problem solving/MySolutionInCourse.java
@@ -599,15 +599,89 @@ public static String readAsString(InputStream inputStream, Charset charset) thro
 }
 
 /*
+Дан сериализуемый класс Animal:
 
+class Animal implements Serializable {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Animal) {
+            return Objects.equals(name, ((Animal) obj).name);
+        }
+        return false;
+    }
+}
+Реализуйте метод, который из переданного массива байт восстановит массив объектов Animal. Массив байт устроен следующим образом. Сначала идет число типа int, записанное при помощи ObjectOutputStream.writeInt(size). Далее подряд записано указанное количество объектов типа Animal, сериализованных при помощи ObjectOutputStream.writeObject(animal).
+Если вдруг массив байт не является корректным представлением массива экземпляров Animal, то метод должен бросить исключение java.lang.IllegalArgumentException.
+Причины некорректности могут быть разные. Попробуйте подать на вход методу разные некорректные данные и посмотрите, какие исключения будут возникать. Вот их-то и нужно превратить в IllegalArgumentException и выбросить. Если что-то забудете, то проверяющая система подскажет. Главное не глотать никаких исключений, т.е. не оставлять нигде пустой catch.
 */
-
-
+public static Animal[] deserializeAnimalArray(byte[] data) {
+    Animal[] animal;
+    try(ObjectInputStream ois=new ObjectInputStream(new ByteArrayInputStream(data)))
+    {
+        int size=ois.readInt();
+        if (size<0) throw new IllegalArgumentException("non correct data[]");
+        animal=new Animal[size];
+        for(int i=0; i<size; i++)
+        {
+            animal[i]=(Animal) ois.readObject();
+        } 
+        return animal;
+    }
+    catch(IllegalArgumentException ex){ ex.printStackTrace(); }
+    catch(Exception ex){ throw new IllegalArgumentException("non correct data[]"); }
+    return null;
+}
 /*
+Напишите программу, читающую текст из System.in и выводящую в System.out сумму всех встреченных в тексте вещественных чисел с точностью до шестого знака после запятой. Числом считается последовательность символов, отделенная от окружающего текста пробелами или переводами строк и успешно разбираемая методом Double.parseDouble.
 
+На этот раз вам надо написать программу полностью, т.е. объявить класс (с именем Main — таково ограничение проверяющей системы), метод main, прописать все import'ы.
+
+Sample Input 1:
+
+1 2 3
+Sample Output 1:
+
+6.000000
+Sample Input 2:
+
+a1 b2 c3
+Sample Output 2:
+
+0.000000
+Sample Input 3:
+
+-1e3
+18 .111 11bbb
+Sample Output 3:
+
+-981.889000
 */
-
-
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.*;
+public class Main {
+    public static void main(String[] args)
+    {
+       Scanner sc=new Scanner(System.in);
+	   List<String> list=new ArrayList<String>();
+       while(sc.hasNext())
+       {
+          list.add(sc.next()); 
+       } 
+	   Double sum=list.stream()
+			.filter((s)->s.matches("[+-]?([0-9]*[.e])?[0-9]+$"))
+			.mapToDouble((x)->Double.valueOf(x))
+			.sum();
+	   
+       System.out.println(String.format("%,6f",sum));
+    }
+}
 /*
 
 */
